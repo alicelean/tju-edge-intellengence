0ef682043139335eddae487b384fefff1330f1be
alice <623660923@qq.com>
2021-10-21 09:32:48 +0800

    change path

--- sh/perform.sh
+++ sh/perform.sh
@@ -5,6 +5,6 @@ n_nodes=5
 int=1
 while(( $int<=$n_nodes ))
 do
-    /usr/local/bin/python3.7  /Users/alice1/Desktop/tju.edu.com/tju-edge-intellengence/client.py&
+    /Users/alice/opt/anaconda3/envs/tensorflow/bin/python  /Users/alice/tju.com/python/tju-edge-intellengence/client.py&
      let "int++"
 done
